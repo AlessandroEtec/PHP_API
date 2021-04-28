3bc74562e2d18a1ef2de93820f2b403014c4f602
Alessandro Ap. Antonio <alessandro.antonio@etec.sp.gov.br>
2021-04-28 07:47:57 -0300

    Criação do arquivo

---
+++ conexao.php
@@ -0,0 +1,11 @@
+<?php
+
+$conexao = new mysqli("localhost:3307", "root", "");
+$conexao->set_charset("UTF8");
+if ($conexao->connect_error) {
+    die("Falha ao conectar: " . $conexao->connect_error);
+}
+if (!$conexao->select_db("api_produto")) {
+    die("O Banco de dados não existe");
+}
+?>
